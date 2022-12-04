88a427dc20f771ac16114317e5d361e295a9f3a9
naps&snacks <j.antoniogarcia1130@gmail.com>
2022-12-03 20:19:53 -0600

    updates to script.js

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Work Day Scheduler Starter Code
-This code contains the files for a a workday scheduler. 
+
+## Description
+Files contain code for a basic hourly workday scheduler. 
+
+## Installation
+This code runs in a web browser
+
+## Usage 
+Utilizes Moment.js as a date and time library, Bootstrap for CSS features, and jquery. 
+
+
+## Image 
--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 //logic for buttons
 
+$(document).ready(function(){
+
+    $(':button').on('click', function (e) {
+        e.preventDefault();
+        var value = $('description').val();
+        console.log(value);
+        
+
+// load data to local storage
+localStorage.setItem(value);
+
+});
+});
 
 //logic to store data in timeblocks
 
 
 //logic for local storage
+
+
 
 
 //will display current day and month, from moment.js
